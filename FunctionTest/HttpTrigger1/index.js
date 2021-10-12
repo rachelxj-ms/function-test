a68a4f7893c8a738385de03265bfee13700783bf
rachelxj-ms <66855967+rachelxj-ms@users.noreply.github.com>
2021-10-12 11:03:03 +0800

    Update index.js

--- FunctionTest/HttpTrigger1/index.js
+++ FunctionTest/HttpTrigger1/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('test JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
